ed73df726f3842683578abdab88483a2a65a0d94
hejun1990 <jun_hejudy@sina.cn>
2017-10-09 11:24:55 +0800

    首次提交

---
+++ src/main/java/com/astarpath/controller/IndexpageController.java
@@ -0,0 +1,15 @@
+package com.astarpath.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hejun on 2017/10/9.
+ */
+@Controller
+public class IndexpageController {
+    @RequestMapping("/astarsearch")
+    public String gotoAStarSearch() {
+        return "astar_search";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta HTTP-EQUIV="Refresh" CONTENT="0;URL=astarsearch">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
